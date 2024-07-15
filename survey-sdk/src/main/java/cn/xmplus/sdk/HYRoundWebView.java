3b723ae2f20b946e25e01a19eb0616e05bd48df9
杨军 <yangjun@surveyplus.cn>
2024-07-15 09:11:36 +0800

    check

--- survey-sdk/src/main/java/cn/xmplus/sdk/HYRoundWebView.java
+++ survey-sdk/src/main/java/cn/xmplus/sdk/HYRoundWebView.java
@@ -30,11 +30,11 @@ public class HYRoundWebView extends WebView {
         invalidate();
     }
 
-    @Override
-    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
-        super.onScrollChanged(l, t, oldl, oldt);
-        Log.d("surveySDK", "onScrollChanged");
-    }
+//    @Override
+//    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+//        super.onScrollChanged(l, t, oldl, oldt);
+////        Log.d("surveySDK", "onScrollChanged");
+//    }
 
     @Override
     protected void onDraw(Canvas canvas) {
